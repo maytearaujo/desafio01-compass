a83361380c4ccf4af21b041bb495c799df645153
Maytê <maytearaujo@yahoo.com.br>
2024-04-22 15:27:38 -0300

    Validações

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,56 +1,82 @@
-// Salvando informações no local storage subscribe
+const emailRegex = new RegExp(
+  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+);
+
 let formSubscribe = document.getElementById("form-subscribe");
 
-
-// formContactUs.addEventListener("submit", (e) => {
 formSubscribe.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let emailSubscribe = document.getElementById("emailSubscribe").value;
+  let emailSubscribe = document.getElementById("emailSubscribe").value;
 
+  if (emailSubscribe === "" || emailRegex.test(emailSubscribe) == false) {
+    document.getElementById("verificarEmail").innerHTML =
+      "Por favor, preencha o e-mail";
+  } else {
     let emails = new Array();
 
-    if (localStorage.hasOwnProperty("emails")){
-        emails = JSON.parse(localStorage.getItem('emails'));
+    if (localStorage.hasOwnProperty("emails")) {
+      emails = JSON.parse(localStorage.getItem("emails"));
     }
 
-    emails.push({emailSubscribe, emailSubscribe})
+    emails.push({ emailSubscribe, emailSubscribe });
 
     localStorage.setItem("emails", JSON.stringify(emails));
-    
-    alert ("Thank you for subscribing.");
-    
-    document.getElementById('emailSubscribe').value='';
 
-})
+    alert("Thank you for subscribing.");
 
+    document.getElementById("emailSubscribe").value = "";
+    document.getElementById("verificarEmail").innerHTML = "";
+  }
+});
 
 // Salvando informações no local storage Form Contact Us
 let formContactUs = document.getElementById("contact-us");
 
 formContactUs.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    let firstName = document.getElementById("firstName").value;
-    let lastName = document.getElementById("lastName").value;
-    let emailAddress = document.getElementById("emailAddress").value;
-    let message = document.getElementById("message").value;
+  e.preventDefault();
 
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let emailAddress = document.getElementById("emailAddress").value;
+  let message = document.getElementById("message").value;
+
+  if (firstName == "") {
+    document.getElementById("verificarCampos").innerHTML =
+      "Please, check the first name";
+    // alert("Nome")
+  } else if (lastName === "") {
+    document.getElementById("verificarCampos").innerHTML = "";
+    document.getElementById("verificarCampos").innerHTML =
+      "Please, check the last name";
+    //   alert("Sobrenome")
+  } else if (emailAddress === "" || emailRegex.test(emailAddress) == false) {
+    document.getElementById("verificarCampos").innerHTML = "";
+    document.getElementById("verificarCampos").innerHTML =
+      "Please, check the e-mail";
+    // alert("email")
+  } else if (message === "") {
+    document.getElementById("verificarCampos").innerHTML = "";
+    document.getElementById("verificarEmail").innerHTML =
+      "Please, check the message";
+    //   alert("mensagem")
+  } else {
+    document.getElementById("verificarCampos").innerHTML = "";
     let usuarios = new Array();
 
-    if (localStorage.hasOwnProperty("usuarios")){
-        usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    if (localStorage.hasOwnProperty("usuarios")) {
+      usuarios = JSON.parse(localStorage.getItem("usuarios"));
     }
 
-    usuarios.push({firstName, lastName, emailAddress, message})
-
+    usuarios.push({ firstName, lastName, emailAddress, message });
 
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-    alert('Welcome, thank you for registering.');
+    alert("Welcome, thank you for registering.");
 
-    document.getElementById('firstName').value = '';
-    document.getElementById('lastName').value = '';
-    document.getElementById('emailAddress').value = '';
-    document.getElementById('message').value = '';
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("emailAddress").value = "";
+    document.getElementById("message").value = "";
+  }
 });
